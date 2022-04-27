3e761f02bee8a5fc9b7732a67577c5820030452f
Tom Augspurger <taugspurger@microsoft.com>
2022-04-26 21:20:09 -0500

    Fixup slim, strip down CIL ensemble example

--- scripts/slim.py
+++ scripts/slim.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
+# TODO: optional compression
+# TODO: proper CLI
 import os
+import subprocess
+import pathlib
 import sys
 import nbformat
 import azure.storage.blob
 import base64
+import tempfile
 
 
 
-def replace_images(nb, credentail):
+def replace_images(nb, path, credential):
     cc = azure.storage.blob.ContainerClient(
         "https://ai4edatasetspublicassets.blob.core.windows.net",
         "assets",
         credential=credential,
     )
-    content_settings = azure.storage.blob.ContentSettings(conent_type="image/png")
+    content_settings = azure.storage.blob.ContentSettings(content_type="image/png")
 
     for cell in nb["cells"]:
         if cell["cell_type"] == "code":
@@ -25,9 +30,15 @@ def replace_images(nb, credentail):
                     output["metadata"].pop("needs_background", None)
                     output["output_type"] = "display_data"
                     png = base64.b64decode(b64png)
-                    url = "https://ai4edatasetspublicassets.blob.core.windows.net/assets"
-                    name = f"notebook-output/customizable-rtc-sentine1.ipynb/{cell['execution_count']}.png"
-                    cc.upload_blob(name, png, content_settings=content_settings, overwrite=True)
+                    with tempfile.TemporaryDirectory() as td:
+                        p = pathlib.Path(str(td)) / "data.png"
+                        dst = p.with_suffix(".tiny.png")
+                        p.write_bytes(png)
+                        subprocess.check_call(["pngquant", str(p), "--output", str(dst)])
+
+                        url = "https://ai4edatasetspublicassets.blob.core.windows.net/assets"
+                        name = f"notebook-output/{path.replace('/', '-')}/{cell['execution_count']}.png"
+                        cc.upload_blob(name, dst.read_bytes(), content_settings=content_settings, overwrite=True)
                     output["data"]["text/html"] = f'<img src="{url}/{name}"/>'
 
 
@@ -35,5 +46,5 @@ if __name__ == "__main__":
     path = sys.argv[1]
     credential = os.environ["PLANETARY_COMPUTER_EXAMPLES_SAS"]
     nb = nbformat.read(path, as_version=4)
-    replace_images(nb, credentail=credential)
+    replace_images(nb, path, credential)
     nbformat.write(nb, path)
